5fd875f79774ca95f37d222d85eb6f730c1173ef
Marcelo Barreto <marcelobarretojunior@gmail.com>
2021-04-02 22:28:16 -0300

    feat: add http module

---
+++ lib/via_cep/http.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module ViaCep
+  module HTTP
+    def self.get(path:, query: {})
+      uri = URI(BASE_URL)
+      uri.path = "/ws/#{path}/json"
+      uri.query = URI.encode_www_form(query)
+
+      Net::HTTP.get_response(uri)
+    end
+
+    def self.was_successful?(request)
+      request.code === '200'
+    end
+  end
+end

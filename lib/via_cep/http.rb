a1661ca7a412fd57c09494e69d71fd87bc15c9e3
Matheus Toledo <matheustoledosjc@gmail.com>
2022-03-28 17:33:41 -0300

    Update http.rb

--- lib/via_cep/http.rb
+++ lib/via_cep/http.rb
@@ -9,8 +9,8 @@ module ViaCep
     #
     def self.get(path:, query: {})
       uri = URI(BASE_URL)
-      uri.path = "/ws/#{URI.encode(path)}/json"
-      uri.query = URI.encode_www_form(query)
+      uri.path = "/ws/#{URI.encode_www_form_component(path)}/json"
+      uri.query = URI.encode_www_form_component(query)
       Net::HTTP.get_response(uri)
     end
 
